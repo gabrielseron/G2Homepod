15dbd24329782387400773a158ed8827b21f688d
augustinmbt <augustinmbath@gmail.com>
2019-02-21 12:00:50 +0100

    update weee almost done!!!

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,22 @@
-var $sliderHomepod = $('#sliderHomepod')
-$sliderHomepod.carousel('pause')
-var radioColors = document.querySelectorAll(".radio-color")
-radioColors.forEach(function($color, index){
-    $color.addEventListener('change', function() {
-        if(this.checked) {
-            $('#sliderHomepod').carousel(index)
-        } 
-    })
-})
+$(document).ready(function () {
+    // If on buy page execute this script
+   if (window.location.pathname == '/buy.html') {
+        // Select DOM slider
+        var $sliderHomepod = $('#sliderHomepod')
+        // Stop autoplay
+        $sliderHomepod.carousel('pause')
+        // Get all DOM radio buttons
+        var radioColors = document.querySelectorAll(".radio-color")
+        radioColors.forEach(function ($color, index) {
+            // Listen to changes on each radio buttons
+            $color.addEventListener('change', function () {
+                // When this one is selected move to the correct slide
+                if (this.checked) {
+                    $('#sliderHomepod').carousel(index)
+                }
+            })
+        })
 
-$('#sliderHomepod').carousel(1)
+        $sliderHomepod.carousel(1)
+    }
+})
